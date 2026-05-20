ae11bcc0fa4a8b06880b574449959150f09126de
lou-m1508 <doanmanhgh@gmail.com>
2026-05-20 15:31:25 +0700

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,9 @@ export const botConfig = {
     activities: [
       {
         // Text users will see (example: "Playing /help | Titan Bot").
-        name: "bot server meomeogaugau",
+        name: "bot server meomeogaugau
+          
+          https://discord.gg/meomeogaugau",
         // Activity type number (0 = Playing).
         type: 4, 
       },
